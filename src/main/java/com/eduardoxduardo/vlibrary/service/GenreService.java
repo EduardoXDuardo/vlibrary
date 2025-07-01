c5208ac51097df493035ff173283ab8bfa5780b9
EduardoXDuardo <luiz.e.silva@usp.br>
2025-07-01 10:56:21 -0300

    feat(genre): Implement find all and find by id methods in Genre Service

--- src/main/java/com/eduardoxduardo/vlibrary/service/GenreService.java
+++ src/main/java/com/eduardoxduardo/vlibrary/service/GenreService.java
@@ -5,10 +5,12 @@ import com.eduardoxduardo.vlibrary.dto.response.GenreResponseDTO;
 import com.eduardoxduardo.vlibrary.mapper.GenreMapper;
 import com.eduardoxduardo.vlibrary.model.entities.Genre;
 import com.eduardoxduardo.vlibrary.repository.GenreRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class GenreService {
@@ -29,10 +31,17 @@ public class GenreService {
     }
 
     @Transactional(readOnly = true)
-    public List<GenreResponseDTO> findAllGenres() {
+    public List<GenreResponseDTO> getAllGenres() {
+        List<Genre> genres = genreRepository.findAll();
+        return genres.stream()
+                .map(genreMapper::toDto)
+                .collect(Collectors.toList());
     }
 
     @Transactional(readOnly = true)
-    public GenreResponseDTO findGenreById(Long id) {
+    public GenreResponseDTO getGenreById(Long id) {
+        Genre genre = genreRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Genre not found with id: " + id));
+        return genreMapper.toDto(genre);
     }
 }
